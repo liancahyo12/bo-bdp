4b492429d5e880ed25d47f4e92e383c8aafac094
Lian Cahyo <lian.cahyo@gmail.com>
2022-03-02 05:57:26 +0000

    tambah format surat, notif surat masuk, update form surat keluar

--- public/assets/vendor/boilerplate/unduhformat.js
+++ public/assets/vendor/boilerplate/unduhformat.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('#jenis_surat').on('change', function() {
         document.getElementById("unduh-format").innerHTML = "<div class='input-group-prepend'> <span class='input-group-text'><span class='fas fa-file'></span></span> </div> <a target='_blank' href='/surat-keluar-buat-format/"+this.value+"' ><button class='btn btn-secondary' form='a'>Unduh Format Surat</button></a>"
+        document.getElementById("ket").innerHTML = "<p>Note : Format dapat diubah sesuai kebutuhan namun kata yang terdapat karakter ${} tidak perlu diubah</p>"
     });
     $('input[type=radio][name="lampiran_radio"]').change(function() {
         if ( this.value == '2')
