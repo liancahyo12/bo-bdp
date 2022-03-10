4505408d7705806b36eddbb47ecf0aa281b354de
Lian Cahyo <lian.cahyo@gmail.com>
2022-03-10 09:39:33 +0700

    fix unduh lampiran review dep

--- app/Http/Controllers/ReviewdepclosingController.php
+++ app/Http/Controllers/ReviewdepclosingController.php
@@ -177,7 +177,7 @@ class ReviewdepclosingController extends Controller
     }
     public function unduh_lampiran($id)
     {
-        if (departemen::where('reviewerdep_id', Auth::user()->id)->first()->id == closing::where('id', $id)->first()->departemen_id || Auth::user()->departemen_id == closing::where('id', $id)->first()->departemen_id) {
+        if (departemen::where('id', closing::where('id', $id)->first()->departemen_id)->first()->reviewerdep_id == Auth::user()->id || Auth::user()->departemen_id == closing::where('id', $id)->first()->departemen_id) {
             $file= Storage::disk('local')->get(closing::where([['id', '=', $id], ['status', '=', 1]])->value('lampiran'));
             return (new Response($file, 200))
                 ->header('Content-Type', 'application/pdf');
