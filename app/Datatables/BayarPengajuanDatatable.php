00e4ee1d005d8e678bb788edb9836ea37eff559e
Lian Cahyo <lian.cahyo@gmail.com>
2022-03-28 11:49:40 +0700

    fix bayar table

--- app/Datatables/BayarPengajuanDatatable.php
+++ app/Datatables/BayarPengajuanDatatable.php
@@ -7,6 +7,7 @@ use Sebastienheyd\Boilerplate\Datatables\Column;
 use Sebastienheyd\Boilerplate\Datatables\Datatable;
 use App\Models\pengajuan;
 use Auth;
+use DB;
 
 class BayarPengajuanDatatable extends Datatable
 {
@@ -14,15 +15,22 @@ class BayarPengajuanDatatable extends Datatable
 
     public function datasource()
     {
-        return pengajuan::leftJoin('jenis_pengajuans', 'pengajuans.jenis_pengajuan_id', 'jenis_pengajuans.id')->whereRaw('pengajuans.status = 1 and review_status=2 and reviewdep_status=2 and approve_status=2')->orderByDesc('pengajuans.updated_at')->get(['pengajuans.id',
-        'pengajuan',
-        'tgl_pengajuan',
-        'jenis_pengajuan',
-        'jenis_pengajuan_id',
-        'no_pengajuan',
-        'bayar_status',
-        'bayar_time',
-        'approve_time',]);
+        return pengajuan::leftJoin('jenis_pengajuans', 'pengajuans.jenis_pengajuan_id', 'jenis_pengajuans.id')->leftJoin('isi_pengajuans', 'isi_pengajuans.pengajuan_id', 'pengajuans.id')->whereRaw('any_value(pengajuans.status) = 1 and any_value(review_status)=2 and any_value(reviewdep_status)=2 and any_value(approve_status)=2')->groupBy('isi_pengajuans.pengajuan_id')->orderByRaw('any_value(pengajuans.updated_at) desc')->get([DB::raw('any_value(pengajuans.id) as  id'),
+        DB::raw('any_value(tgl_pengajuan) as tgl_pengajuan'),
+        DB::raw('any_value(jenis_pengajuan) as jenis_pengajuan'),
+        DB::raw('any_value(pengajuans.jenis_pengajuan_id) as jenis_pengajuan_id'),
+        DB::raw('any_value(no_pengajuan) as no_pengajuan'),
+        DB::raw('any_value(review_status) as review_status'),
+        DB::raw('any_value(review_time) as review_time'),
+        DB::raw('any_value(reviewdep_status) as reviewdep_status'),
+        DB::raw('any_value(reviewdep_time) as reviewdep_time'),
+        DB::raw('any_value(approve_status) as approve_status'),
+        DB::raw('any_value(approve_time) as approve_time'),
+        DB::raw('any_value(send_time) as send_time'),
+        DB::raw('any_value(send_status) as send_status'),
+        DB::raw('any_value(bayar_time) as bayar_time'),
+        DB::raw('any_value(bayar_status) as bayar_status'),
+        DB::raw('ifnull(any_value(transaksi), any_value(jenis_transaksi)) as transaksi')]);
     }
 
     public function setUp()
@@ -32,21 +40,6 @@ class BayarPengajuanDatatable extends Datatable
     public function columns(): array
     {
         return [
-            Column::add('Waktu Approve')
-                ->data('approve_time'),
-
-            Column::add('Id')
-                ->data('id'),
-
-            Column::add('No Pengajuan')
-                ->data('no_pengajuan'),
-            
-            Column::add('Jenis Pengajuan')
-                ->data('jenis_pengajuan'),
-
-            Column::add('Tgl Pengajuan')
-                ->data('tgl_pengajuan'),
-
             Column::add('Status Bayar')
                 ->width('40px')
                 ->data('custom_status', function (pengajuan $pengajuan) {
@@ -64,6 +57,18 @@ class BayarPengajuanDatatable extends Datatable
 
             Column::add('Waktu Bayar')
                 ->data('bayar_time'),
+
+            Column::add('No Pengajuan')
+                ->data('no_pengajuan'),
+
+            Column::add('Jenis Pengajuan')
+                ->data('jenis_pengajuan'),
+            
+            Column::add('Pengajuan')
+                ->data('transaksi'),
+
+            Column::add('Tgl Pengajuan')
+                ->data('tgl_pengajuan'),
                 
             Column::add('Aksi')
             ->actions(function(pengajuan $pengajuan) {
